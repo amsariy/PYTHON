877c3f628d8fa40e8bc2feb0cb37ff1f5b4bf4d1
ambarsari-3003 <ambarsariyy@gmail.com>
2022-10-23 18:55:43 +0700

    Python 01

---
+++ Tanya Jawab Proposisi.py
@@ -0,0 +1,238 @@
+YATIDAK = "A"
+TIDAKTIDAK = "B"
+BELUMDITEMUKAN = "c"
+TIDAKADA = "D"
+a = "benar"
+b = "salah"
+d = "ya"
+e = "tidak"
+
+
+
+# SOAL 1
+print("I. GAJAH LEBIH BESAR DARIPADA TIKUS")
+print("1. Apakah ini sebuah pernyataan?")
+print("ketikan ya/tidak")
+jawaban1 = input("= ")
+if jawaban1  == d:
+    print("ANDA BENAR!👍")
+elif jawaban1 == e:
+    print("ANDA SALAH!")
+else:
+    print("SALAH!Jawab ya/tidak")
+print("")
+
+print("2. Apakah ini sebuah proposisi?")
+print("ketikan ya/tidak")
+jawaban2 = input("= ")
+if jawaban2  == d:
+    print("ANDA BENAR!👍")
+elif jawaban2 == e:
+    print("ANDA SALAH!")
+else:
+    print("SALAH!Jawab ya/tidak")
+print("")
+
+print("3. Apakah nilai kebenaran dari proposisi ini?")
+print("ketikan benar/salah")
+jawaban3 = input("= ")
+if jawaban3  == a:
+    print("ANDA BENAR!👍")
+elif jawaban3 == b:
+    print("ANDA SALAH!")
+else:
+    print("SALAH!Jawab benar/salah")
+print("")
+
+
+# SOAL 2
+print("II. 520 < 111")
+print("1. Apakah ini sebuah pernyataan?")
+print("ketikan ya/tidak")
+jawab1 = input("= ")
+if jawab1  == d:
+    print("ANDA BENAR!👍")
+elif jawab1 == e:
+    print("ANDA SALAH!")
+else:
+    print("SALAH! Jawab ya/tidak")
+print("")
+
+print("2. Apakah ini sebuah proposisi?")
+print("ketikan ya/tidak")
+jawab2 = input("= ")
+if jawab2  == d:
+    print("ANDA BENAR!👍")
+elif jawab2 == e:
+    print("ANDA SALAH!")
+else:
+    print("SALAH! Jawab benar/salah")
+print("")
+
+print("3. Apakah nilai kebenaran dari proposisi ini?")
+print("ketikan benar/salah")
+jawab3 = input("= ")
+if jawab3  == b:
+    print("ANDA BENAR!👍")
+elif jawab3 == a:
+    print("ANDA SALAH!")
+else:
+    print("SALAH! Jawab benar/salah")
+print("")
+
+
+# SOAL 3
+print("III. y > 5")
+print("1. Apakah ini sebuah pernyataan?")
+print("ketikan ya/tidak")
+jwb1 = input("= ")
+if jwb1  == d:
+    print("ANDA BENAR!👍")
+elif jwb1 == e:
+    print("Anda salah!")
+else:
+    print("SALAH! Jawab ya/tidak")
+print("")
+
+print("2. Apakah ini sebuah proposisi?")
+print("ketikan ya/tidak")
+jwb2 = input("= ")
+if jwb2  == e:
+    print("ANDA BENAR!👍")
+elif jwb2 == d:
+    print("Anda salah!")
+else:
+    print("SALAH! Jawab ya atau tidak")
+print("")
+
+print("3. Apakah nilai kebenaran dari proposisi ini?")
+print("Pilih a/b/c")
+print("a. benar")
+print("b. salah")
+print("c. belum ditentukan")
+jwb3 = input("= ")
+if jwb3  == BELUMDITEMUKAN:
+    print("ANDA BENAR!👍")
+else:
+    print("ANDA SALAH!")
+print("")
+
+
+# SOAL 4
+print("IV. SEKARANG TAHUN 2004 DAN 99 < 5")
+print("1. Apakah ini sebuah pernyataan?")
+print("ketikan ya/tidak")
+answer1 = input("= ")
+if answer1  == d:
+    print("ANDA BENAR!👍")
+elif answer1 == e:
+    print("Anda salah!")
+else:
+    print("SALAH! Jawab ya/tidak")
+print("")
+
+print("2. Apakah ini sebuah proposisi?")
+print("ketikan ya/tidak")
+answer2 = input("= ")
+if answer2  == d:
+    print("ANDA BENAR!👍")
+elif answer2 == e:
+    print("ANDA SALAH!")
+else:
+    print("SALAH! Jawab ya/tidak")
+print("")
+
+print("3. Apakah nilai kebenaran dari proposisi ini?")
+print("ketikan benar/salah")
+answer3 = input("= ")
+if answer3  == b:
+    print("ANDA BENAR!👍")
+else:
+    print("ANDA SALAH!")
+print("")
+
+
+# SOAL 5
+print("V. DIHARAP UNTUK TIDAK TIDUR SELAMA KULIAH ONLINE DAN OFFLINE")
+print("1. Apakah ini sebuah pernyataan?")
+print("Pilih A/B/C/D")
+print("A. YA,TIDAK")
+print("B. TIDAK,TIDAK")
+print("C. YA, YA")
+print("D. TIDAK ADA")
+answr1 = input("= ")
+if answr1  == YATIDAK:
+    print("ANDA BENAR!👍")
+elif answr1 == TIDAKTIDAK:
+    print("ANDA SALAH!")
+else:
+    print("ANDA SALAH!")
+print("")
+
+print("2. Apakah ini sebuah proposisi?")
+print("ketikan A/B/C/D")
+print("A. YA,TIDAK")
+print("B. TIDAK,TIDAK")
+print("C. YA, YA")
+print("D. TIDAK ADA")
+answr2 = input("= ")
+if answr2  == TIDAKTIDAK:
+    print("ANDA BENAR!👍")
+elif answr2 == YATIDAK:
+    print("Anda salah!")
+else:
+    print("SALAH!")
+print("")
+
+print("3. Apakah nilai kebenaran dari proposisi ini?")
+print("ketikan A/B/C/D")
+print("A. YA,TIDAK")
+print("B. TIDAK,TIDAK")
+print("C. YA, YA")
+print("D. TIDAK ADA")
+answr3 = input("= ")
+if answr3  == TIDAKADA:
+    print("ANDA BENAR!👍")
+else:
+    print("ANDA SALAH!")
+print("")
+
+
+# SOAL 6
+print("VI. 6. X < Y Jika dan hanya jika Y > X")
+print("P <=> Q")
+print("1. Apakah ini sebuah pernyataan?")
+print("ketikan ya/tidak")
+jawaban1 = input("= ")
+if jawaban1  == d:
+    print("ANDA BENAR!👍")
+elif jawaban1 == e:
+    print("Anda salah!")
+else:
+    print("SALAH! Jawab ya/tidak")
+print("")
+
+print("2. Apakah ini sebuah proposisi?")
+print("ketikan ya/tidak")
+jawaban2 = input("= ")
+if jawaban2  == d:
+    print("ANDA BENAR!👍")
+elif jawaban2 == e:
+    print("Anda salah!")
+else:
+    print("SALAH! Jawab ya/tidak")
+print("")
+
+print("3. Apakah nilai kebenaran dari proposisi ini?")
+print("ketikan benar/salah")
+jawaban3 = input("= ")
+if jawaban3  == a:
+    print("ANDA BENAR!👍")
+elif jawaban3 == b:
+    print("ANDA SALAH!")
+else:
+    print("SALAH! Jawab benar/salah")
+print("")
+print("SELESAI❤️️")
+
+()
